3fe4f64b2888cf324606cab2012217c496c12fff
Aaron Dubbers <dubbers.aaron@outlook.com>
2023-03-27 13:26:39 +0200

    level 2 teleport script added

---
+++ George_Cloney/Assets/scripts/Level2_TP.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Level2_TP : MonoBehaviour
+{
+    void Update()
+    {
+
+    }
+
+    //Upon collision with another GameObject, this GameObject will reverse direction
+    private void OnTriggerEnter(Collider other)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level_2");
+    }
+}
